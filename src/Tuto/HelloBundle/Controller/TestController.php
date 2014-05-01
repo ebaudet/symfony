0f9b83da48dd2ac83c088dce694c3d8bc3e58844
Emilien BAUDET <ebaudet@e1r3p12.42.fr>
2014-05-01 19:13:22 +0200

    symfony install

---
+++ src/Tuto/HelloBundle/Controller/TestController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Tuto\HelloBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+
+class TestController extends Controller
+{
+    function indexAction() {
+        return new Response("Ceci est un test !");
+    }
+}
